b7eaed203ee0affbd6c109465001332d0a860653
Mattis Bodynek <mattis.bodynek@h-its.org>
2025-07-14 15:22:39 +0200

    First working prototype

--- ensemblify/manager.py
+++ ensemblify/manager.py
@@ -1,60 +1,80 @@
 from abc import ABC
 from ensemblify import utils
-from Bio.SeqRecord import SeqRecord
+from Bio import SeqRecord
+from Bio import SeqIO
+from Bio import AlignIO
+from Bio.Align import MultipleSeqAlignment
 from pathlib import Path
+from tempfile import NamedTemporaryFile
+import random
+import os
 
-class BaseManager(ABC):
-    # Job: execute cmd 
-    pass
 
-    def compute(self):
-        # Compute alignments associated with given config
-        # Output should always be a collection of .fasta files in the correct output directory 
-        pass
-
-class DefaultManager(BaseManager):
-    def __init__(self, config:dict, tool_name:str, in_file:Path, out_dir:Path, log_file:Path, threads:int):
+class Manager(ABC):
+    def __init__(self, config:dict, tool_name:str, in_file:Path, out_dir:Path, log_file:Path, threads:int, shuffle:bool=False):
         self.config = config
         self.tool_name = tool_name
         self.in_file = in_file
         self.out_dir = out_dir
         self.log_file = log_file
         self.threads = threads
-        self.variants = utils.get_option_variants(self.config)
+        self.shuffle = shuffle
+        self.variants = utils.get_option_variants(self.config["ensemble"][tool_name]["params"])
 
-    def compute(self):
+
+    @classmethod
+    def save_ensemble(cls, ensemble_dict: dict[tuple, MultipleSeqAlignment], out_dir:Path):
+        for keys, msa in ensemble_dict.items():
+            AlignIO.write(msa, out_dir / ("-".join(map(str, keys)) + ".fasta"), "fasta")
+
+
+    def compute(self) -> dict[tuple, MultipleSeqAlignment]:
+        # Compute alignments associated with given config
+        # Output should always be a collection of .fasta files in the correct output directory 
+        raise NotImplementedError()
+
+
+class DefaultManager(Manager):
+    def compute(self) -> dict[tuple, MultipleSeqAlignment]:
+        self.alignments = {}
         for i, options in enumerate(self.variants):
-            out_file = self.output / f"msa.{i}.fasta"
+            out_file = self.out_dir / f"msa.{i}.fasta"
             
+            aligner = self.config["ensemble"][self.tool_name]["aligner"]
             # Mafft does not have an -output flag, we have to use stdout as intended
-            if not "{output}" in self.config["aligner"]["cmd"]:
+            if not "{output}" in self.config["aligners"][aligner]["cmd"]:
                 stdout = out_file
             else:
                 # All other aligners have dedicated -output flags, using log file as stdout to avoid bugs
-                stdout = self.log
+                stdout = self.log_file
             
-            in_file = 
-            cmd = utils.format_cmd(self.config, self.tool_name, options, self.threads, in_file, out_file,)
-            utils.run_cmd(
-                cmd, outfile=stdout, logfile=self.log, log_write_mode="a"
-            )
-            if self._benchmarking:
-                stop = perf_counter()
-                self._perf_dict[str(tool_enum)].append(stop - start)
-            alignment = Alignment(out_file)
-            alignments.append(alignment)
+            if self.shuffle:
+                seq_records = list(SeqIO.parse(self.in_file, "fasta"))                
+                random.shuffle(seq_records)
+                temp_file = NamedTemporaryFile("w", delete=False)
+                in_file = Path(temp_file.name)
+                SeqIO.write(seq_records, in_file, format="fasta")
+            else:
+                in_file = self.in_file
 
-        ensemble = Ensemble(alignments)
-        ensemble.to_efa(out_file)
-        return ensemble
+            cmd = utils.format_cmd(self.config, self.tool_name, options, self.threads, in_file, out_file)
+            utils.run_cmd(cmd, outfile=stdout, logfile=self.log_file, log_write_mode="a")
+
+            msa = AlignIO.read(out_file, "fasta")
+            self.alignments[(self.tool_name, i)] = msa
+            
+            if self.shuffle:
+                temp_file.close()
+    
+        return self.alignments
 
 
-class Muscle5Manager(BaseManager):
+class Muscle5Manager(Manager):
     pass
 
 
 
-def infer_manager(aligner:str):
+def infer_manager(aligner:str) -> Manager:
     match(aligner):
         case "Muscle5":
             return Muscle5Manager
@@ -63,116 +83,56 @@ def infer_manager(aligner:str):
         # TODO: Add more manager classes for other alignment programs
 
 
-class Aligner:
-    def __init__(
-        self,
-        dataset: list[SeqRecord],
-        seed: int = 0,
-        # benchmarking: bool = False,
-    ):
-        self._dataset = dataset
-        self._seed = seed
-        # self._benchmarking = benchmarking
-        # if benchmarking:
-        #     self._perf_dict = defaultdict(list)
+   
 
-    def compute(
-        self,
-        config: dict,
-        out_file: Path,
-        log_file: Path,
-        threads: int,
-    ):
-        if config.aligner == "default":
-            return self._compute_muscle5(tool_enum, out_file, log_file, threads)
-        else:
-            return self._compute_singletons(tool_enum, out_file, log_file, threads)
+    # def _compute_muscle5(
+    #     self,
+    #     muscle5: ToolEnum,
+    #     out_file: pathlib.Path,
+    #     log_file: pathlib.Path,
+    #     threads: int,
+    # ):
+    #     options = ToolEnum.get_option_variants(muscle5)
+    #     if len(options) > 1:
+    #         raise NotImplementedError(
+    #             "Combinatorial options not implemented for Muscle5"
+    #         )
+    #     options = options[0]
+    #     # Using -stratified for 16 alignments at once
+    #     cmd = self._format_cmd(muscle5, out_file, options, threads)
+    #     if self._benchmarking:
+    #         start = perf_counter()
+    #     features.utils.run_cmd(
+    #         cmd, outfile=log_file, logfile=log_file, log_write_mode="a"
+    #     )
+    #     if self._benchmarking:
+    #         stop = perf_counter()
+    #         self._perf_dict[str(muscle5)].append(stop - start)
+    #     ensemble = Ensemble.from_efa(AlignerEnum.MUSCLE5.path, out_file)
+    #     return ensemble
 
-    def _compute_singletons(
-        self,
-        tool_enum: ToolEnum,
-        out_file: pathlib.Path,
-        log_file: pathlib.Path,
-        threads: int,
-    ):
-        alignments = []
-        # get number of option combinations:
-        for i, options in enumerate(ToolEnum.get_option_variants(tool_enum)):
-            file_name = options.replace(SPACE_CHAR, "_").strip(" ")
-            if not file_name:
-                file_name = "_"
-            msa_out_file = out_file.parent / f"msa.{i}.{file_name}.fasta"
-            # Mafft does not have an -output flag, we have to use stdout as intended
-            if tool_enum.aligner == AlignerEnum.MAFFT:
-                stdout = msa_out_file
-            else:
-                # All other aligners have dedicated -output flags, using log file as stdout to avoid bugs
-                stdout = log_file
+    # def _format_cmd(
+    #     self,
+    #     tool_enum: ToolEnum,
+    #     out_file: pathlib.Path,
+    #     options: str,
+    #     threads: int,
+    #     index: int = None,
+    # ):
+    #     shuffle_seqs = index is not None
+    #     if shuffle_seqs:
+    #         sequence_records = self._dataset.sequences
+    #         random.shuffle(sequence_records)
+    #         seq_file = out_file.parent / f"sequences.{index}.fasta"
+    #         SeqIO.write(sequence_records, seq_file, format="fasta")
+    #     else:
+    #         seq_file = self._dataset.file_path
 
-            cmd = self._format_cmd(tool_enum, msa_out_file, options, threads, index=i)
-            if self._benchmarking:
-                start = perf_counter()
-            features.utils.run_cmd(
-                cmd, outfile=stdout, logfile=log_file, log_write_mode="a"
-            )
-            if self._benchmarking:
-                stop = perf_counter()
-                self._perf_dict[str(tool_enum)].append(stop - start)
-            alignment = Alignment(msa_out_file)
-            alignments.append(alignment)
-
-        ensemble = Ensemble(alignments)
-        ensemble.to_efa(out_file)
-        return ensemble
-
-    def _compute_muscle5(
-        self,
-        muscle5: ToolEnum,
-        out_file: pathlib.Path,
-        log_file: pathlib.Path,
-        threads: int,
-    ):
-        options = ToolEnum.get_option_variants(muscle5)
-        if len(options) > 1:
-            raise NotImplementedError(
-                "Combinatorial options not implemented for Muscle5"
-            )
-        options = options[0]
-        # Using -stratified for 16 alignments at once
-        cmd = self._format_cmd(muscle5, out_file, options, threads)
-        if self._benchmarking:
-            start = perf_counter()
-        features.utils.run_cmd(
-            cmd, outfile=log_file, logfile=log_file, log_write_mode="a"
-        )
-        if self._benchmarking:
-            stop = perf_counter()
-            self._perf_dict[str(muscle5)].append(stop - start)
-        ensemble = Ensemble.from_efa(AlignerEnum.MUSCLE5.path, out_file)
-        return ensemble
-
-    def _format_cmd(
-        self,
-        tool_enum: ToolEnum,
-        out_file: pathlib.Path,
-        options: str,
-        threads: int,
-        index: int = None,
-    ):
-        shuffle_seqs = index is not None
-        if shuffle_seqs:
-            sequence_records = self._dataset.sequences
-            random.shuffle(sequence_records)
-            seq_file = out_file.parent / f"sequences.{index}.fasta"
-            SeqIO.write(sequence_records, seq_file, format="fasta")
-        else:
-            seq_file = self._dataset.file_path
-
-        kwargs = dict(
-            aligner=str(tool_enum.aligner.path),
-            input=str(seq_file),
-            output=str(out_file),
-            options=options,
-            threads=threads,
-        )
-        return ToolEnum.format_cmd(tool_enum, kwargs)
+    #     kwargs = dict(
+    #         aligner=str(tool_enum.aligner.path),
+    #         input=str(seq_file),
+    #         output=str(out_file),
+    #         options=options,
+    #         threads=threads,
+    #     )
+    #     return ToolEnum.format_cmd(tool_enum, kwargs)
--- ensemblify/utils.py
+++ ensemblify/utils.py
@@ -1,5 +1,8 @@
 import itertools
-
+import tempfile
+import subprocess
+from typing import Literal
+from pathlib import Path
 _SPACER = "§"
 
 
@@ -12,16 +15,17 @@ def equals_none(string:str):
     return False
 
 
-def get_option_variants(config:dict):
+def get_option_variants(params:dict):
         """Returns a list with the cartesian product of all combinations of options, formatted as strings."""
-        if config.get("params") is None:
+        if params is None:
+            raise ValueError("Missing 'params' entry.")
             # Return empty string to avoid cmd error (necessary ?)
-            return [""]
+            # return [""]
         
         variants = []
         base_options = []
         product_options = {}
-        for key, val in config["params"].items(): 
+        for key, val in params.items(): 
             if equals_none(str(val)):
                 # If val is none: simple flag
                 base_options.append(key)
@@ -41,3 +45,75 @@ def get_option_variants(config:dict):
             variants.append(variant)
         variants = [_SPACER.join(variant) for variant in variants]
         return variants
+
+
+def format_cmd(
+    config: dict,
+    tool_name:str,
+    options: str,
+    threads: int,
+    in_file: Path,
+    out_file: Path,
+):
+
+    aligner_name = config["ensemble"][tool_name]["aligner"]
+    kwargs = dict(
+        aligner=str(config["aligners"][aligner_name]["path"]),
+        input=str(in_file),
+        output=str(out_file),
+        options=options,
+        threads=threads,
+    )
+    cmd_template = config["aligners"][aligner_name]["cmd"]
+    cmd_template = _SPACER.join(map(str.strip, cmd_template.split(" ")))
+    temp = cmd_template.format(**kwargs).split(_SPACER)
+    commands = []
+    for command in temp:
+        section = command.strip(" ")
+        if section:
+            commands.append(section)
+    return commands
+
+
+
+
+
+def run_cmd(
+    cmd: list[str],
+    outfile: Path,
+    logfile: Path = None,
+    log_write_mode: Literal["a", "w"] = "a",
+    out_write_mode: Literal["a", "w"] = "w",
+) -> None:
+    """
+    Runs a given command using the subprocess Python module. The results of the run are stored in the given `outfile`
+    and all logging and errors are written to `logfile` using the given write mode.
+
+    Parameters
+    ----------
+    cmd : list[str]
+        Command to run as list of strings.
+    outfile : pathlib.Path
+        File where to store the output of the given command execution in.
+    logfile : pathlib.Path
+        File where to store all logging/errors in.
+    log_write_mode : str
+        Write mode to open the logfile with.
+        Allowed options are `'a'` (append logs to existing logs) and `'w'` (overwrite all existing logs).
+
+    """
+    if log_write_mode not in ["a", "w"]:
+        raise ValueError(
+            f"Invalid write mode for logfile given: {log_write_mode}. Allowed options are 'a' (append) and 'w' (write)."
+        )
+    if not logfile:
+        logfile = Path(tempfile.NamedTemporaryFile("w").name)
+    with logfile.open(log_write_mode) as log, outfile.open(out_write_mode) as out:
+        try:
+            subprocess.run(cmd, stdout=out, stderr=log, check=True, encoding="utf-8")
+        except subprocess.CalledProcessError as e:
+            raise RuntimeError(
+                f"Error running CMD: {' '.join(cmd)}"
+                f"Check the logfile {logfile.absolute()} for details."
+                f"Logfile: {logfile.open().read()}"
+            )
